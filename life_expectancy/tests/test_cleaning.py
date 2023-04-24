81ec78165ef52d73600648d7d1441b0b73807c9f
dgenio <diogofcul@hotmail.com>
2023-04-24 15:14:17 +0100

    Update test_cleaning.py
    Remove argument on test as suggested

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -7,7 +7,7 @@ from . import OUTPUT_DIR
 
 def test_clean_data(pt_life_expectancy_expected):
     """Run the `main` function and compare the output to the expected output"""
-    main(country_code="PT")
+    main()
     pt_life_expectancy_actual = pd.read_csv(
         OUTPUT_DIR / "pt_life_expectancy.csv"
     )
