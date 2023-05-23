86d944f602da809b61002a4635f80e8b99f851cb
dandrsantos <diogo.ansantos@nos.pt>
2023-05-23 12:15:21 +0100

    Create fixture for initial data

--- life_expectancy/tests/conftest.py
+++ life_expectancy/tests/conftest.py
@@ -21,3 +21,13 @@ def run_before_and_after_tests() -> None:
 def pt_life_expectancy_expected() -> pd.DataFrame:
     """Fixture to load the expected output of the cleaning script"""
     return pd.read_csv(FIXTURES_DIR / "pt_life_expectancy_expected.csv")
+
+
+@pytest.fixture(scope="session")
+def eu_life_expectancy_data() -> pd.DataFrame:
+    """Fixture to load data from life expectancy """
+    return pd.read_csv(
+        FIXTURES_DIR / "pt_life_expectancy_expected.tsv",
+        sep="\t",
+        header=0
+    )
