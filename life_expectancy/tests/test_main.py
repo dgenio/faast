256e6fb5f4b1f65073d3b6281cb251d34c174845
dandrsantos <diogo.ansantos@nos.pt>
2023-05-30 12:27:48 +0100

    Remove message

--- life_expectancy/tests/test_main.py
+++ life_expectancy/tests/test_main.py
@@ -8,7 +8,6 @@ from . import FIXTURES_DIR
 def test_main(pt_life_expectancy_expected):
     """Run the `main` function and compare the output to the expected output"""
     with mock.patch.object(pd.DataFrame, "to_csv") as mock_to_csv:
-        mock_to_csv.side_effect = print("Message: Dataframe saved to CSV")
         cleaned_data = main(path=FIXTURES_DIR / "eu_life_expectancy_input.tsv")
         pd.testing.assert_frame_equal(
             cleaned_data.reset_index(drop=True), pt_life_expectancy_expected
